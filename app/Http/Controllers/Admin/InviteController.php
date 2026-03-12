06bcb37aa02fba732631db9680a4eb058d6cac02
Saeed Hosan <saeedhosansh@gmail.com>
2026-03-12 16:53:16 +0600

    fix code style

--- app/Http/Controllers/Admin/InviteController.php
+++ app/Http/Controllers/Admin/InviteController.php
@@ -1,9 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class InviteController extends Controller
 {
