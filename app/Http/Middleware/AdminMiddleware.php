f0da9013a37adc2f4bdf0b9bbbea727f0d784184
Saeed Hosan <saeedhosansh@gmail.com>
2026-03-12 14:41:15 +0600

    feat: add admin middleware

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use App\Enums\UserType;
+use Closure;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($this->isAdmin($request->user())) {
+            return $next($request);
+        }
+
+        if ($this->isCustomer($request->user())) {
+            // redirect to customer dashboard
+        }
+
+        if ($this->isAdministrator($request->user())) {
+            // redirect to administrator dashboard
+        }
+
+        abort(Response::HTTP_FORBIDDEN);
+    }
+
+    private function isAdmin(?Authenticatable $user)
+    {
+        return $user instanceof Authenticatable && $user->type === UserType::admin;
+    }
+
+    private function isCustomer(?Authenticatable $user)
+    {
+        return $user instanceof Authenticatable && $user->type === UserType::customer;
+    }
+
+    private function isAdministrator(?Authenticatable $user)
+    {
+        return $user instanceof Authenticatable && $user->type === UserType::administrator;
+    }
+}
