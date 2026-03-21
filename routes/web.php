728fce6e7576c3ad357df83fa69d88e2d148e8e3
Saeed Hosan <saeedhosansh@gmail.com>
2026-03-22 00:50:05 +0600

    fix: customer route name prefix

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ require __DIR__.'/settings.php';
 Route::middleware('auth')->group(function () {
     Route::name('super.')->middleware('super')->prefix('super')->group(base_path('routes/super.php'));
     Route::name('admin.')->middleware('admin')->prefix('admin')->group(base_path('routes/admin.php'));
-    Route::name('customer')->middleware('customer')->group(base_path('routes/customer.php'));
+    Route::name('customer.')->middleware('customer')->group(base_path('routes/customer.php'));
     Route::name('user.')->group(base_path('routes/user.php'));
 });
