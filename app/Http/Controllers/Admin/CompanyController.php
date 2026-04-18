37a914688e2125ee4a2dfe2bf211e75884f25120
Saeed Hosan <saeedhosansh@gmail.com>
2026-04-18 14:25:53 +0600

    feat: display company list in admin dashboard matching app style

--- app/Http/Controllers/Admin/CompanyController.php
+++ app/Http/Controllers/Admin/CompanyController.php
@@ -4,65 +4,52 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Admin;
 
+use App\Domain\Company\CompanyService;
 use App\Http\Controllers\Controller;
 use App\Models\Company;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CompanyController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    public function __construct(
+        private readonly CompanyService $service
+    ) {}
+
     public function index(): Factory|View
     {
-        return view('admin.company.index');
+        $companies = $this->service->paginateForAdmin(Auth::user());
+
+        return view('admin.companies.index', ['companies' => $companies]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create(): Factory|View
     {
-        return view('admin.company.create');
+        return view('admin.companies.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request): void
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Company $company): Factory|View
     {
-        return view('admin.company.show', ['company' => $company]);
+        return view('admin.companies.show', ['company' => $company]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Company $company): Factory|View
     {
-        return view('admin.company.edit', ['company' => $company]);
+        return view('admin.companies.edit', ['company' => $company]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Company $company): void
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Company $company): void
     {
         //
